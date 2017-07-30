fd529bfa2664babf9719b17dc8ef38f2aff1e36a
Collins Arku Yao <arkucollins@gmail.com>
2017-07-30 00:00:45 +0000

    Create Check.java

---
+++ Even-Odd/Check.java
@@ -0,0 +1,12 @@
+public class Check {
+public static void main (String [] argos){
+   int number=5;
+    // Check if the number is  divisible by 2
+    if(number % 2 == 0)
+        System.out.println ("%d is even.", number);
+    else
+        System.out.println("%d is odd.", number);
+
+    return 0;
+   }
+  } 
