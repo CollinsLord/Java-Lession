7319052ee6b94a6b9b79c0eba82da66dae127322
Collins <arkucollins@gmail.com>
2017-08-25 22:35:38 +0000

    String Demo

---
+++ Strings/StringD.java
@@ -0,0 +1,9 @@
+public class StringD{
+
+   public static void main(String args[]) {
+      String lnd = "My first string program in java";
+      int len = lnd.length();
+      System.out.println( "String Length is : " + len );
+
+   }
+}
